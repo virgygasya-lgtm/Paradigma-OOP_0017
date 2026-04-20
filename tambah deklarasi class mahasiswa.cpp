4951297ad5ba3d074ce51a3f8e078075073841ea
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-20 10:27:52 +0700

    tambah deklarasi class mahasiswa.cpp

---
+++ tambah deklarasi class mahasiswa.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+    
+    public:
+    int nim;
+    string nama;
+    float nilai;
+
+    void printdata() {
+        cout << "NIM: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+
+    }
+};  //batas class
