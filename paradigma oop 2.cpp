2989297f6fcc0ed0b83833295dd4b9c150187c66
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-04-20 11:28:48 +0700

    paradigma oop 2.cpp

--- paradigma oop 2.cpp
+++ paradigma oop 2.cpp
@@ -1,4 +1,20 @@
+#include<iostream>
+using namespace std;
 
+class barang{
+    public:
+    string nama;
+    int jumlah;
+    string kartegori;
+    int tanggalProduksi;
+
+    void printData(){
+        cout << "Nama: " << nama << endl;
+        cout << "jumlah: " << jumlah << endl;
+        cout << "kartegori: " << kartegori << endl;
+        cout << "tanggal produksi: " << tanggalProduksi  << endl;
+    }
+};
 int main ()
 {
     barang elekronik;
